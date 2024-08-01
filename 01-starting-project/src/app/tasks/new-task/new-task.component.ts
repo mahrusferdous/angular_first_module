8ce7bc021d38e58d8582a54192f75e07e2ac6a74
Mahrus Ferdous <mahrusferdous@gmail.com>
2024-08-01 14:15:03 -0400

    chore: Remove unused import in new-task component

--- 01-starting-project/src/app/tasks/new-task/new-task.component.ts
+++ 01-starting-project/src/app/tasks/new-task/new-task.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { type NewTaskData } from '../task.model';
 
